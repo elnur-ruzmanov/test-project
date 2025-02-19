2cb42d16c7913bd3f9622e8b3be1386650fb0437
elnur-ruzmanov <elnuruz13@gmail.com>
2025-02-19 18:37:47 +0500

    Update gitignore

---
+++ src/test/java/SmokeApiTests.java
@@ -0,0 +1,32 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SmokeApiTests {
+    @Test
+    void simpleTest(){
+        String body= """
+                {
+                    "id": 0,
+                    "username": "string",
+                    "firstName": "string",
+                    "lastName": "string",
+                    "email": "string",
+                    "password": "string",
+                    "phone": "string",
+                    "userStatus": 0
+                  }""";
+       Response respose = given()
+                    .header("accept", "application/json")
+                     .header("Content-Type", "application/json")
+                    .baseUri("https://petstore.swagger.io/v2/")
+                .when()
+                    .body(body)
+                    .post("user")
+                .andReturn();
+       int actualCode = respose.getStatusCode();
+        Assertions.assertEquals(200, actualCode);
+    }
+}
